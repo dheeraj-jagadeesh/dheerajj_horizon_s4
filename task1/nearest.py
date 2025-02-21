54bbd3f5c3e2147ee57f75b79406bd786aa4dd3b
dheeraj-jagadeesh <dheeraj.heisenlock@gmail.com>
2025-02-21 22:09:09 +0530

    Added Inline Comments

--- task1/nearest.py
+++ task1/nearest.py
@@ -1,41 +1,55 @@
 import matplotlib.pyplot as plt
 
-# Function to get user input for points
+# This function is used get user input for points
 def get_points():
     points = []
-    n = int(input("Enter the number of points: "))
-    for i in range(n):
-        x, y = map(float, input(f"Enter coordinates for point {i + 1} (x y): ").split())
+    numpoints = int(input("Enter the number of points? "))
+
+    for i in range(numpoints):
+        x = int(input(f"Enter point x{i+1} = "))
+        y = int(input(f"Enter point y{i+1} = "))
         points.append((x, y))
+    
     return points
 
-# Function to calculate distance
-def euclidean_distance(p1, p2):
+# This is used to calculate distance
+def calc_distance(p1, p2):
     return ((p1[0] - p2[0]) ** 2 + (p1[1] - p2[1]) ** 2) ** 0.5
 
-# Function to find and draw lines to the nearest neighbors
-def draw_nearest_neighbors(points):
-    used_edges = set()
+# This is used to find and draw lines to the nearest neighbors
+def draw_the_line(points):
+    used_edgings = set()
     plt.figure()
-    plt.scatter([p[0] for p in points], [p[1] for p in points], color='red', zorder=5)
+    unique_points = set(points)
+    x_points = []
+    y_points = []
+    for point in points:
+        x_points.append(point[0])
+        y_points.append(point[1])
+    plt.scatter(x_points, y_points,color = "red", zorder=2)
 
-    for i, point in enumerate(points):
+    i = 0
+    for point in points:
         distances = []
-        for j, other_point in enumerate(points):
+        j = 0
+        for next_point in points:
             if i != j:
-                dist = euclidean_distance(point, other_point)
+                dist = calc_distance(point, next_point)
                 distances.append((dist, j))
+            j += 1
+        distances.sort()
+            
 
-        distances.sort()  # Sort by distance
 
-        # Find the nearest neighbor that hasn't been connected with a duplicate edge
-        for dist, neighbor_idx in distances:
-            edge = tuple(sorted((i, neighbor_idx)))
-            if edge not in used_edges:
-                used_edges.add(edge)
-                plt.plot([point[0], points[neighbor_idx][0]],
-                         [point[1], points[neighbor_idx][1]], 'b-', zorder=1)
+        # Finding the nearest neighbor that hasn't been connected with a duplicate edge
+        for dist, neighbour in distances:
+            edging = tuple(sorted((i, neighbour)))
+            if edging not in used_edgings:
+                used_edgings.add(edging)
+                plt.plot([point[0], points[neighbour][0]],
+                         [point[1], points[neighbour][1]], 'b-', zorder=1)
                 break  # Move to the next point after finding the nearest unused neighbor
+        i += 1
 
     plt.xlabel('X-axis')
     plt.ylabel('Y-axis')
@@ -45,7 +59,7 @@ def draw_nearest_neighbors(points):
 # Main function
 def main():
     points = get_points()
-    draw_nearest_neighbors(points)
+    draw_the_line(points)
 
 if __name__ == '__main__':
     main()
